8a465df01817ec7530f5afeb4aecd435301dece6
cdroom <softmg@gmail.com>
2020-12-26 15:50:29 -0300

    actualizacion favicon y cambios jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,21 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    $('.modal').on("hidden.bs.modal", function () {
+       setTimeout(function(){$('[data-toggle="tooltip"]').tooltip('hide'); }, 10);
+    });
+
+    $("a").click(function(event){
+        if (this.hash !== "") {
+           event.preventDefault();
+           var gato = this.hash;
+             $("html, body").animate({
+               scrollTop: $(gato).offset().top
+               }, 800, function(){
+                window.location.hash = gato;
+               });
+         }
+       });
+
 
     $("a").click(function(event){
         if (this.hash !== "") {
@@ -30,4 +46,10 @@ $(function () {
         $('.navbar-nav>li>a').on('click', function(){
             $('.navbar-collapse').collapse('hide');
         });
-})
+
+        $('.nav-link').click(function(e) {
+            $('.nav-item').removeClass('active');
+            $(this).parent().addClass('active');
+          });
+
+    })
